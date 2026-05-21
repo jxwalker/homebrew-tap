695b5bc806fa3fb780ebb16073d6b4deb24490a6
James Walker <james@jameswalker.org.uk>
2026-05-21 10:52:44 +0100

    modfetch 0.9.0 (#5)

--- Formula/modfetch.rb
+++ Formula/modfetch.rb
@@ -3,20 +3,20 @@ class Modfetch < Formula
   homepage "https://github.com/jxwalker/modfetch"
   license "MIT"
 
-  release = "v0.8.1"
+  release = "v0.9.0"
 
   if OS.mac?
     url "https://github.com/jxwalker/modfetch/releases/download/#{release}/modfetch_darwin_universal",
         using: :nounzip
-    sha256 "7edf759efc8c5982147d2213acc6b0139c9cc67b2f81f10eb3ad2e698a43e73f"
+    sha256 "61f8f180f41b1fdd2e5eee5edd3edda5a2b668f1312db9a9fa6b910e97c1a489"
   elsif Hardware::CPU.arm?
     url "https://github.com/jxwalker/modfetch/releases/download/#{release}/modfetch_linux_arm64",
         using: :nounzip
-    sha256 "a8de023093967cc6b5c6c6ac61faddb687bc6d1eb6635bb1163f79fbd5cb83d7"
+    sha256 "5e6859b2eda89063304dccbf2ffeebaf8dd0d69c58cd7f65a03b4d4eb0aaac98"
   else
     url "https://github.com/jxwalker/modfetch/releases/download/#{release}/modfetch_linux_amd64",
         using: :nounzip
-    sha256 "09f8bede51811fa5e4e61a5437b2654058ef96f0ee6db231178d03186e8f9201"
+    sha256 "7e9854541455317e7db188a43b5df78f4b1f633cb3cd14c4b5ef39984d2ec27c"
   end
 
   def install
